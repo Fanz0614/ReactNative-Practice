15b1ae7ab0352e4508b99d512a228cd9cea6ff9b
Jacky Zhang <jackyzh0614@gmail.com>
2022-01-15 13:11:03 +1100

    amodify app.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const enteredGoalHandler = (value) => {
+    setEnteredGoal(value)
+  }
+  const addGoalHandler = () => {
+    alert(enteredGoal)
+  }
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput placeholder="Course Goal" style={styles.input} onChangeText={enteredGoalHandler} value={enteredGoal} />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View />
     </View>
